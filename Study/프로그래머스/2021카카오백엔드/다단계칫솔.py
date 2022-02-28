4d5e3abf3dd571177a5be43cae07089f9208e0b2
bbnerino <bbnerino@gmail.com>
2022-02-28 18:39:27 +0900

    algo

---
+++ Study/프로그래머스/2021카카오백엔드/다단계칫솔.py
@@ -0,0 +1,5 @@
+enroll = ["john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young"]
+referral = ["-", "-", "mary", "edward", "mary", "mary", "jaimie", "edward"]
+seller = ["young", "john", "tod", "emily", "mary"]
+amount = [12, 4, 2, 5, 10]
+
