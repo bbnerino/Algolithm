da97579aba140b510ffa4efc137796fca01c1d09
임현홍 <bbnerino@gmail.com>
2022-07-14 00:01:44 +0900

    소수의 연속합

--- Study/7월/1644_소수의연속합.py
+++ Study/7월/1644_소수의연속합.py
@@ -4,7 +4,7 @@ sys.stdin = open('input.txt')
 N = int(input())
 
 sosu = []
-for i in range(2,N):
+for i in range(2,N+1):
     flag = True
     for j in range(2,i):
         if i%j ==0:
@@ -12,6 +12,14 @@ for i in range(2,N):
     if flag:
         sosu.append(i)
 
-nums = list(range(N))
 
-print(sosu)
+count = 0
+for i in range(1,len(sosu)):
+    for j in range(len(sosu)-i+1):
+        combi = sosu[j:j+i]
+        if sum(combi) == N:
+
+            count+=1
+
+
+print(count)
