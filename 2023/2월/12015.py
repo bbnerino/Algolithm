b62ebeb1b54440a67af4fde7818825facfc81326
임현홍 <bbnerino@gmail.com>
2023-02-01 15:18:43 +0900

    LIS 알고리즘

--- 2023/2월/12015.py
+++ 2023/2월/12015.py
@@ -3,4 +3,22 @@ sys.stdin = open("./2023/2월/input.txt")
 
 N = int(input())
 num_list = list(map(int,input().split()))
-print(num_list)
+
+dp = [0]
+# [10,20,10,30,20,50]
+for num in num_list :
+  if num > dp[-1]:
+    dp.append(num)
+  else:
+    left = 0
+    right = len(dp)
+    
+    while left < right:
+      mid = (left+right)//2
+      if dp[mid] < num:
+        left = mid+1
+      else:
+        right = mid
+    dp[right] = num
+
+print(len(dp)-1)
