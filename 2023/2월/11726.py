7f63f182aa0c89cf742ff547286eaed4a0d10708
임현홍 <bbnerino@gmail.com>
2023-02-09 09:17:01 +0900

    종이네칸

--- 2023/2월/11726.py
+++ 2023/2월/11726.py
@@ -6,7 +6,7 @@ sys.stdin = open("./2023/2월/input.txt")
 N = int(input())
 dp  = [0 for _ in range(N+1)]
 dp[1] = 1
-if(N>2):
+if(N>=2):
   dp[2] = 2
   for i in range(3,N+1):
     dp[i] = dp[i-1]+dp[i-2]
